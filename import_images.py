697de0d7379b19f2e1adef48b347dfdf5909a255
lky1901246487@gmail.com <17377083@buaa.edu.cn>
2023-11-07 13:53:21 +0800

    update utils & import_image

---
+++ import_images.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+from glob import glob
+from datetime import datetime
+
+from pymongo.collection import Collection
+from tqdm import tqdm
+
+import clip_model
+import utils
+
+
+def import_single_image(filename: str, model: clip_model.CLIPModel,
+                        config: dict, mongo_collection: Collection, copy=True):
+
+    index = filename.find(".", 2)
+    filetype = filename[index+1:]
+    if filetype is None:
+        print("skip file:", filename)
+        return
+
+    image_feature, image_size = model.get_image_feature(filename)
+    if image_feature is None:
+        print("skip file:", filename)
+        return
+    image_feature = image_feature.astype(config['storage-type'])
+
+    if copy:
+        md5hash = utils.calc_md5(filename)
+        new_basename = md5hash + '.' + filetype
+        new_full_path = utils.get_full_path(
+            config['import-image-base'], new_basename)
+
+        if os.path.isfile(new_full_path):
+            print("duplicate file:", filename)
+            return
+
+        shutil.copy2(filename, new_full_path)
+        stat = os.stat(new_full_path)
+    else:
+        stat = os.stat(filename)
+        new_full_path = filename
+
+    image_mtime = datetime.fromtimestamp(stat.st_mtime)
+    image_datestr = image_mtime.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+    # save to mongodb
+    document = {
+        'filename': new_full_path,
+        'extension': filetype,
+        'height': image_size[1],
+        'width': image_size[0],
+        'filesize': stat.st_size,
+        'date': image_datestr,
+        'feature': image_feature.tobytes(),
+    }
+
+    x = mongo_collection.insert_one(document)
+    return x
+
+
+def import_dir(base_dir: str, model: clip_model.CLIPModel,
+               config: dict, mongo_collection: Collection, copy=False):
+    list_file = os.listdir(base_dir)
+
+    filelist = [base_dir+'/'+f for f in list_file]
+    filelist = [f for f in filelist if os.path.isfile(f)]
+
+    for filename in tqdm(filelist):
+        import_single_image(filename, model, config,
+                            mongo_collection, copy=copy)
+
+
+def main():
+    config = utils.get_config()
+    mongo_collection = utils.get_mongo_collection()
+    model = clip_model.get_model()
+    import_dir("./imgs", model, config, mongo_collection, True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import hashlib
+from functools import lru_cache
+
+import pymongo
+from pymongo.collection import Collection
+import clip
+
+
+@lru_cache(maxsize=1)
+def get_config():
+    with open('config.json') as json_data_file:
+        c = json.load(json_data_file)
+
+    if "clip-model" in c:
+        assert c["clip-model"] in clip.available_models()
+    if "device" in c:
+        assert c["device"] in ["cuda", "cpu"]
+    return c
+
+
+def get_feature_size(model_name):
+    if model_name == "ViT-B/32":
+        return 512
+    elif model_name == "ViT-L/14":
+        return 768
+    else:
+        raise ValueError("Unknown model")  # TODO: complete this table
+
+
+def get_file_type(image_path):
+    libmagic_output = os.popen("file '" + image_path + "'").read().strip()
+    libmagic_output = libmagic_output.split(":", 1)[1]
+    if "PNG" in libmagic_output:
+        return "png"
+    if "JPEG" in libmagic_output:
+        return "jpg"
+    if "GIF" in libmagic_output:
+        return "gif"
+    if "PC bitmap" in libmagic_output:
+        return "bmp"
+    return None
+
+
+@lru_cache(maxsize=1)
+def get_mongo_collection() -> Collection:
+    config = get_config()
+    mongo_client = pymongo.MongoClient(
+        "mongodb://{}:{}/".format(config['mongodb-host'], config['mongodb-port']))
+    mongo_collection = mongo_client[config['mongodb-database']
+                                    ][config['mongodb-collection']]
+    return mongo_collection
+
+
+def calc_md5(filepath):
+    with open(filepath, 'rb') as f:
+        md5 = hashlib.md5()
+        while True:
+            data = f.read(4096)
+            if not data:
+                break
+            md5.update(data)
+        return md5.hexdigest()
+
+
+def get_full_path(basedir, basename):
+    md5hash, ext = basename.split(".")
+    return "{}/{}/{}".format(basedir, ext, basename)
