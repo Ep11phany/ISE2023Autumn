d0ddd914546c4d3629c9674dcde2f8c495d2e9b9
gangaoliu <18256715510@163.com>
2023-11-16 17:58:39 +0800

    feat: mod retriever

--- utils/retriever.py
+++ utils/retriever.py
@@ -1,29 +1,26 @@
 from PIL import Image
-from langchain.vectorstores import faiss as FAISS
+from utils.vecstore import MYFAISS
 from transformers import CLIPProcessor, CLIPModel
 
-class Retriever(object):
+class Retriever(MYFAISS):
     def __init__(self, args):
+        super().__init__(args.db_path, args.matedata_path, args.dim)
         self.model, self.processor = self.__build_model(args.model_name_or_path)
-        self.db = self.__load_db(args.db_path, self.model, self.processor)
 
     def __build_model(self, model_name_or_path):
         model = CLIPModel.from_pretrained(model_name_or_path)
         processor = CLIPProcessor.from_pretrained(model_name_or_path)
         return model, processor
 
-    def __load_db(self, db_path, model, processor):
-        pass
-        #return FAISS.load_local(db_path, model, processor)
-
     def search(self, text, image, k=1):
+        image_urls = []
         if not text is None:
             text_vec = self.__text2vec(text)
+            image_urls += self.similarity_search(text_vec, k=k)
         if not image is None:
             image_vec = self.__image2vec(image)
-        # vec = self.model.encode(text)
-        # image_url = self.db.similarity_search_with_score(vec, k=k)
-        #return image_url
+            image_urls += self.similarity_search(image_vec, k=k)
+        return image_urls
 
     def __text2vec(self, text):
         inputs = self.processor(text=text, return_tensors="pt")
